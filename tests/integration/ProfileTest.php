b733ec06dfb330bd4144cac404045974ee873288
Stefan Freudenberg <stefan@agaric.com>
2015-08-28 17:22:31 +0200

    Add test case for profile

---
+++ tests/integration/ProfileTest.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @file
+ * Test case for profile
+ */
+
+require_once 'DrupalIntegrationTestCase.php';
+
+class ProfileTest extends DrupalIntegrationTestCase {
+
+  /**
+   * Tests content types exist.
+   */
+  public function testContentTypesExist() {
+    $types = node_type_get_types();
+    $this->assertArrayHasKey('page', $types);
+  }
+
+}
