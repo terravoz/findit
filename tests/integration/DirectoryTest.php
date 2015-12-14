06cfd1f6e1c9dc80d73779317e36f0359c2f44d5
Mauricio Dinarte <drupal@dinarcon.com>
2015-12-14 14:04:32 -0600

    Add directory tests.

---
+++ tests/integration/DirectoryTest.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ * @file
+ * Test case for announcements.
+ */
+require_once 'DrupalIntegrationTestCase.php';
+
+class DirectoryTest extends DrupalIntegrationTestCase {
+
+	/**
+	 * Tests only current announcements are displayed.
+	 */
+	public function testActivity() {
+		$view = views_get_view('directory');
+		$view->set_display('block_activity');
+		$view->execute();
+		$results = $view->result;
+
+		$categories = drupal_json_decode(file_get_contents(DRUPAL_ROOT . '/profiles/findit/data/program_categories.json'));
+
+		$this->assertEquals(count($categories), count($results));
+	}
+
+	/**
+	 * Tests directory view and block configuration.
+	 */
+	public function testViewDirectoryConfiguration() {
+		$views = views_get_view('directory');
+
+		$views_displays = $views->display;
+		$this->assertArrayHasKey('block_activity', $views_displays);
+		$this->assertArrayHasKey('block_organization', $views_displays);
+
+		$_GET['q'] = 'directory';
+		$blocks = block_list('content');
+		$this->assertArrayHasKey('views_directory-block_activity', $blocks);
+		$this->assertArrayHasKey('views_directory-block_organization', $blocks);
+	}
+
+}
