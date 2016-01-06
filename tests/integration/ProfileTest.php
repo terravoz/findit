35d2034182308552931f5deec9916f62aca320cc
Stefan Freudenberg <stefan@agaric.com>
2016-01-06 10:38:37 +0100

    Move tests for languages and translations to a separate test case

--- tests/integration/ProfileTest.php
+++ tests/integration/ProfileTest.php
@@ -332,37 +332,4 @@ class ProfileTest extends DrupalIntegrationTestCase {
     $this->assertEquals(TRANSLATION_ENABLED, variable_get('language_content_type_page'));
   }
 
-  /**
-   * Tests languages are enabled.
-   */
-  public function testLanguagesAreEnabled() {
-    $languages = language_list();
-    $this->assertArrayHasKey('en', $languages);
-    $this->assertArrayHasKey('es', $languages);
-  }
-
-  /**
-   * Tests default language.
-   */
-  public function testDefaultLanguage() {
-    $this->assertEquals('en', language_default()->language);
-  }
-
-  /**
-   * Test language negotiation settings.
-   */
-  public function testLanguageNegotiationSettings() {
-    $language_negotiation = variable_get('language_negotiation_language');
-    $this->assertArrayHasKey('locale-url', $language_negotiation);
-    $this->assertArrayHasKey('locale-browser', $language_negotiation);
-    $this->assertArrayHasKey('language-default', $language_negotiation);
-  }
-
-  /**
-   * Tests main menu is translatable.
-   */
-  public function testMainMenuIsTranslatable() {
-    $menu = menu_load('main-menu');
-    $this->assertEquals(I18N_MODE_MULTIPLE, $menu['i18n_mode']);
-  }
 }
