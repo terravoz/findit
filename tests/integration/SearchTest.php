42bf3107fbb4958565a0aea64cd3afb1ae9ee869
Stefan Freudenberg <stefan@agaric.com>
2016-01-06 11:18:41 +0100

    Format source code

--- tests/integration/SearchTest.php
+++ tests/integration/SearchTest.php
@@ -8,30 +8,31 @@ require_once 'DrupalIntegrationTestCase.php';
 
 class SearchTest extends DrupalIntegrationTestCase {
 
-	/**
-	 * Tests only current announcements are displayed.
-	 */
-	public function testEmptyResults() {
-		$view = views_get_view('search');
-		$view->set_display('page_search');
-		$view->set_arguments(array(33));
-		$view->set_exposed_input(array('type' => array('event', 'program')));
-		$view->execute();
-		$results = $view->result;
+  /**
+   * Tests only current announcements are displayed.
+   */
+  public function testEmptyResults() {
+    $view = views_get_view('search');
+    $view->set_display('page_search');
+    $view->set_arguments(array(33));
+    $view->set_exposed_input(array('type' => array('event', 'program')));
+    $view->execute();
+    $results = $view->result;
 
-		$this->assertEquals(0, count($results));
-	}
+    $this->assertEquals(0, count($results));
+  }
 
-	/**
-	 * Tests only current announcements are displayed.
-	 */
-	public function testProgramResults() {
-		$view = views_get_view('search');
-		$view->set_display('page_search');
-		$view->set_exposed_input(array('type' => array('program')));
-		$view->execute();
-		$results = $view->result;
+  /**
+   * Tests only current announcements are displayed.
+   */
+  public function testProgramResults() {
+    $view = views_get_view('search');
+    $view->set_display('page_search');
+    $view->set_exposed_input(array('type' => array('program')));
+    $view->execute();
+    $results = $view->result;
 
-		$this->assertEquals(1, count($results));
-	}
+    $this->assertEquals(1, count($results));
+  }
+
 }
