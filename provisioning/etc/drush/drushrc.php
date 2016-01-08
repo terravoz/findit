b7861598b73ae494dc62e5de2701a10f3d9e7a15
Stefan Freudenberg <stefan@agaric.com>
2016-01-08 12:24:40 +0100

    Add site-name to global drush options

--- provisioning/etc/drush/drushrc.php
+++ provisioning/etc/drush/drushrc.php
@@ -2,3 +2,4 @@
 // Specify your Drupal core base directory (useful if you use symlinks).
 $options['r'] = '/vagrant/web';
 $options['account-pass'] = 'admin';
+$options['site-name'] = 'Find It Cambridge';
