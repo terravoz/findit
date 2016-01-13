57d59544941e62e48bc2cbe958a6502cd2e3d0ff
Mauricio Dinarte <drupal@dinarcon.com>
2016-01-13 15:52:30 -0600

    RM #2516 Taxonomy migrate parent relationship by id. Update program_categories.

--- tests/integration/TaxonomyTest.php
+++ tests/integration/TaxonomyTest.php
@@ -20,16 +20,16 @@ class TaxonomyTest extends DrupalIntegrationTestCase {
       $vocabularies_names[$vocabulary->machine_name] = $vocabulary->machine_name;
     }
 
+    $this->assertArrayHasKey('accessibility_options', $vocabularies_names);
+    $this->assertArrayHasKey('amenities', $vocabularies_names);
+    $this->assertArrayHasKey('grade_eligibility_options', $vocabularies_names);
+    $this->assertArrayHasKey('location_types', $vocabularies_names);
+    $this->assertArrayHasKey('other_eligibility_options', $vocabularies_names);
     $this->assertArrayHasKey('program_categories', $vocabularies_names);
     $this->assertArrayHasKey('time_day_of_week', $vocabularies_names);
     $this->assertArrayHasKey('time_of_day', $vocabularies_names);
     $this->assertArrayHasKey('time_of_year', $vocabularies_names);
     $this->assertArrayHasKey('time_other', $vocabularies_names);
-    $this->assertArrayHasKey('grade_eligibility_options', $vocabularies_names);
-    $this->assertArrayHasKey('other_eligibility_options', $vocabularies_names);
-    $this->assertArrayHasKey('accessibility_options', $vocabularies_names);
-    $this->assertArrayHasKey('amenities', $vocabularies_names);
-    $this->assertArrayHasKey('location_types', $vocabularies_names);
   }
   
 }
