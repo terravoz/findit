8c2ed451671397bc086944803e9c12cc8dad538a
Stefan Freudenberg <stefan@agaric.com>
2016-01-19 16:56:51 +0100

    Remove id attribute from blocks

--- web/sites/all/themes/findit_cambridge/templates/block.tpl.php
+++ web/sites/all/themes/findit_cambridge/templates/block.tpl.php
@@ -44,7 +44,7 @@
  * @ingroup themeable
  */
 ?>
-<div id="<?php print $block_html_id; ?>" class="<?php print $classes; ?>"<?php print $attributes; ?>>
+<div class="<?php print $classes; ?>">
   <?php print render($title_prefix); ?>
   <?php if ($block->subject): ?>
   <h2<?php print $title_attributes; ?>><?php print $block->subject ?></h2>
