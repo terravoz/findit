34574443c36f8502ca7f0c012299aca0ec32dff1
Stefan Freudenberg <stefan@agaric.com>
2016-01-19 13:44:03 +0100

    Override theme_form_element()

--- web/sites/all/themes/findit_cambridge/template.php
+++ web/sites/all/themes/findit_cambridge/template.php
@@ -18,3 +18,50 @@ function findit_cambridge_css_alter(&$css) {
 function findit_cambridge_menu_tree__main_menu(&$variables) {
   return '<ul class="nav-main">' . $variables['tree'] . '</ul>';
 }
+
+/**
+ * Overrides theme_form_element().
+ */
+function findit_cambridge_form_element(&$variables) {
+  $element = &$variables['element'];
+
+  // This function is invoked as theme wrapper, but the rendered form element
+  // may not necessarily have been processed by form_builder().
+  $element += array(
+    '#title_display' => 'before',
+  );
+
+  $output = '';
+
+  // If #title is not set, we don't display any label or required marker.
+  if (!isset($element['#title'])) {
+    $element['#title_display'] = 'none';
+  }
+  $prefix = isset($element['#field_prefix']) ? '<span class="field-prefix">' . $element['#field_prefix'] . '</span> ' : '';
+  $suffix = isset($element['#field_suffix']) ? ' <span class="field-suffix">' . $element['#field_suffix'] . '</span>' : '';
+
+  switch ($element['#title_display']) {
+    case 'before':
+    case 'invisible':
+      $output .= ' ' . theme('form_element_label', $variables);
+      $output .= ' ' . $prefix . $element['#children'] . $suffix . "\n";
+      break;
+
+    case 'after':
+      $output .= ' ' . $prefix . $element['#children'] . $suffix;
+      $output .= ' ' . theme('form_element_label', $variables) . "\n";
+      break;
+
+    case 'none':
+    case 'attribute':
+      // Output no label and no required marker, only the children.
+      $output .= ' ' . $prefix . $element['#children'] . $suffix . "\n";
+      break;
+  }
+
+  if (!empty($element['#description'])) {
+    $output .= '<div class="description">' . $element['#description'] . "</div>\n";
+  }
+
+  return $output;
+}
