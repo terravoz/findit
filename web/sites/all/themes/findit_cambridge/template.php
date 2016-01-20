4e82cb67fd9a89527536b92d9abc99b8fe2d1a2a
Stefan Freudenberg <stefan@agaric.com>
2016-01-20 22:34:24 +0100

    Theme directory

--- web/sites/all/themes/findit_cambridge/template.php
+++ web/sites/all/themes/findit_cambridge/template.php
@@ -79,3 +79,94 @@ function findit_cambridge_form_element(&$variables) {
 
   return $output;
 }
+
+/**
+ * Overrides theme_item_list().
+ */
+function findit_cambridge_item_list(&$variables) {
+  $items = $variables['items'];
+  $title = $variables['title'];
+  $type = $variables['type'];
+  $attributes = $variables['attributes'];
+
+  // Only output the list container and title, if there are any list items.
+  // Check to see whether the block title exists before adding a header.
+  // Empty headers are not semantic and present accessibility challenges.
+  $output = '';
+  if (isset($title) && $title !== '') {
+    $output .= '<h3>' . $title . '</h3>';
+  }
+
+  if (!empty($items)) {
+    $output .= "<$type" . drupal_attributes($attributes) . '>';
+    $num_items = count($items);
+    $i = 0;
+    foreach ($items as $item) {
+      $attributes = array();
+      $children = array();
+      $data = '';
+      $i++;
+      if (is_array($item)) {
+        foreach ($item as $key => $value) {
+          if ($key == 'data') {
+            $data = $value;
+          }
+          elseif ($key == 'children') {
+            $children = $value;
+          }
+          else {
+            $attributes[$key] = $value;
+          }
+        }
+      }
+      else {
+        $data = $item;
+      }
+      if (count($children) > 0) {
+        // Render nested list.
+        $data .= theme_item_list(array('items' => $children, 'title' => NULL, 'type' => $type, 'attributes' => $attributes));
+      }
+      $output .= '<li' . drupal_attributes($attributes) . '>' . $data . "</li>\n";
+    }
+    $output .= "</$type>";
+  }
+
+  return $output;
+}
+
+/**
+ * Overrides theme_views_tree_inner().
+ */
+function findit_cambridge_views_tree_inner(&$variables) {
+  $options = $variables['options'];
+  $rows = $variables['rows'];
+  $result = $variables['result'];
+  $parent = $variables['parent'];
+  $attributes = array();
+
+  $items = array();
+  foreach ($result as $i => $record) {
+    if ($record->views_tree_parent == $parent) {
+      $variables['parent'] = $record->views_tree_main;
+      if ($parent == 0) {
+        $items[] = array(
+          'data' => '<h3 class="expandable-heading">' . $rows[$i] . '</h3>' . call_user_func(__FUNCTION__, $variables),
+          'class' => array('expandable'),
+        );
+      }
+      else {
+        $items[] = $rows[$i] . call_user_func(__FUNCTION__, $variables);
+      }
+    }
+  }
+
+  if ($parent == 0) {
+    $attributes['class'] = array('expandable-container');
+  }
+
+  return theme('item_list', array(
+    'items' => $items,
+    'type' => $options['type'],
+    'attributes' => $attributes,
+  ));
+}
