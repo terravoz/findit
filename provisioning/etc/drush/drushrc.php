99db4a071f8d11b6de2bc703c7efbf0ece865bbf
Stefan Freudenberg <stefan@agaric.com>
2016-02-17 09:40:31 -0500

    Set variables for drush si

--- provisioning/etc/drush/drushrc.php
+++ provisioning/etc/drush/drushrc.php
@@ -2,4 +2,7 @@
 // Specify your Drupal core base directory (useful if you use symlinks).
 $options['r'] = '/vagrant/web';
 $options['account-pass'] = 'admin';
+$otpions['account-mail'] = 'admin@finditcambridge.org';
 $options['site-name'] = 'Find It Cambridge';
+$options['site-slogan'] = 'Your gateway to children, youth, and family opportunities in Cambridge, Massachusetts.';
+$options['site-mail'] = 'info@finditcambridge.org';
