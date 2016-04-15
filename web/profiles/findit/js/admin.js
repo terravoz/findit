de5aaf9d139008b0f6cd1129d9be19143b3477a4
Mauricio Dinarte <drupal@dinarcon.com>
2016-04-15 12:33:28 -0600

    Fix formatting

--- web/profiles/findit/js/admin.js
+++ web/profiles/findit/js/admin.js
@@ -2,13 +2,13 @@
   Drupal.behaviors.findit = {
     attach: function (context) {
       $('.field-name-field-program-categories .form-checkboxes label').each( function(index) {
-        var label = $( this ).text();
+        var label = $(this).text();
 
-        if ( !( /^-/.test( label ) ) ) {
-          $( this ).siblings( 'input' ).remove();
+        if (!( /^-/.test(label))) {
+          $(this).siblings('input').remove();
         }
         else {
-          $( this ).text( label.replace( /-*/, '' ) );
+          $(this).text(label.replace(/-*/, ''));
         }
       });
     }
