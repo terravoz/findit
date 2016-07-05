eb899be07312b0f64dcf2237627939605c37818c
Mauricio Dinarte <drupal@dinarcon.com>
2016-07-04 20:59:05 -0600

    Allow multiple sortable tables in same page

--- web/sites/all/modules/contrib/tablesorter/tablesortervar.js
+++ web/sites/all/modules/contrib/tablesorter/tablesortervar.js
@@ -6,27 +6,30 @@
 (function ($) {
   Drupal.behaviors.tablesorter = {
     attach: function (context, settings) {
-
       var widgets = [];
       var widgetsZebra = [];
 
       if (settings.tablesorter.zebra == 1) {
-          widgets.push('zebra');
-          widgetsZebra.push(settings.tablesorter.odd);
-          widgetsZebra.push(settings.tablesorter.even);
+        widgets.push('zebra');
+        widgetsZebra.push(settings.tablesorter.odd);
+        widgetsZebra.push(settings.tablesorter.even);
       }
-    $('.tablesorter').once('tablesorter', function() {
-      $(".tablesorter").tablesorter({
-          widgets: widgets,
-          widgetsZebra: {css: widgetsZebra}
-      })
-      if ($("#tablesorter_pager").length != 0) {
-          $(".tablesorter").tablesorterPager({
-              container: $("#tablesorter_pager")
-          });
-      }
-    });
 
+      $('.tablesorter').each(function(idx, table) {
+        $(table).once('tablesorter', function() {
+          $(table).tablesorter({
+            widgets: widgets,
+            widgetsZebra: {
+              css: widgetsZebra
+            }
+          });
+          if ($("#tablesorter_pager").length != 0) {
+            $(table).tablesorterPager({
+              container: $("#tablesorter_pager")
+            });
+          }
+        });
+      });
     }
   };
 })(jQuery);
